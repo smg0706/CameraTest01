9ef812be82c90896c7d427e3bcdbafc7510d3ec9
SM.Guang <guang0706@gmail.com>
2013-05-16 00:19:00 +0900

    Do not used "UIImagePickerController"

---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraTest01
+//
+//  Created by 史 美光 on 13/04/27.
+//  Copyright (c) 2013年 史 美光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CameraTest01
+//
+//  Created by 史 美光 on 13/04/27.
+//  Copyright (c) 2013年 史 美光. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
